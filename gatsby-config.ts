27673e86fbd587c5808d5b5708149b9ad0720f6d
Rômullo Cordeiro <romullocordeiro@gmail.com>
2022-04-05 22:58:42 -0300

    Initial commit from gatsby

---
+++ gatsby-config.ts
@@ -0,0 +1,11 @@
+import type { GatsbyConfig } from "gatsby";
+
+const config: GatsbyConfig = {
+  siteMetadata: {
+    title: `basic-landing-page`,
+    siteUrl: `https://www.yourdomain.tld`
+  },
+  plugins: ["gatsby-plugin-styled-components"]
+};
+
+export default config;
