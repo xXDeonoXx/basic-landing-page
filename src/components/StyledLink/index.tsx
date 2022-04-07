6255d5fb3c6aba0cd234e3420625946e944ae9e6
Rômullo Cordeiro <romullocordeiro@gmail.com>
2022-04-06 21:47:26 -0300

    feat: 100 seo !

--- src/components/StyledLink/index.tsx
+++ src/components/StyledLink/index.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const StyledLink = () => {
   return (
-    <a href='' className='rounded-md px-4 py-2 bg-sky-600 text-white'>
+    <a href='#' className='rounded-md px-4 py-2 bg-sky-600 text-white'>
       Saiba mais
     </a>
   );
